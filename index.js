8b1e8ff2b12dd6fe85c1d57ead9b28b3619738e1
Tobias Koppers <tobias.koppers@googlemail.com>
2012-10-08 14:53:40 +0200

    some fixes for 0.7, inline worker support with fallback

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var webpack = require("webpack");
 var clone = require("clone");
 var path = require("path");
+var EventEmitter = require("events").EventEmitter;
 
 module.exports = function(source) {
 	var loaderSign = this.request.indexOf("!");
 	var remReq = this.request.substr(loaderSign);
 	var options = clone(this.options);
+	var constructor = "new Worker";
 	options.cache = this.options.cache;
 	options.watch = false;
 	options.noWrite = true;
@@ -18,6 +20,11 @@ module.exports = function(source) {
 	options.template = require("./templates/worker");
 	var newBuildin = path.join(__dirname, "buildin");
 	options.resolve.paths.unshift(newBuildin);
+	options.events = new EventEmitter();
+	options.events.on("module", this.options.events.emit.bind(this.options.events, "module"));
+	options.events.on("context-enum", this.options.events.emit.bind(this.options.events, "context-enum"));
+	options.events.on("context", this.options.events.emit.bind(this.options.events, "context"));
+	options.events.on("task", this.options.events.emit.bind(this.options.events, "task-end"));
 	if(this.options.worker) {
 		if(typeof this.options.worker.webpack == "object") {
 			for(var name in this.options.worker.webpack) {
@@ -25,18 +32,32 @@ module.exports = function(source) {
 			}
 		}
 	}
+	var inlineWorker = this.options.worker.inline
 	var self = this;
 	var callback = this.async();
 	webpack(remReq, options, function webpackFinishedInWorkerLoader(err, stats) {
 		if(err) return callback(err);
 		var worker = null;
+		var workerMainContent = null;
 		workerWrites.forEach(function(write) {
 			self.emitFile(write[0].replace(/\[hash\]/g, stats.hash), write[1]);
+			if(inlineWorker && write[0] == options.output && write[1]) {
+				constructor = "require(" + JSON.stringify(path.join(__dirname, "createInlineWorker.js")) + ")";
+				workerMainContent = write[1];
+			}
 		});
 		stats.request = remReq;
 		self.emitSubStats(stats);
+		stats.dependencies.forEach(function(dep) {
+			self.dependency && self.dependency(dep);
+		});
+		stats.loaders.forEach(function(dep) {
+			self.loaderDependency && self.loaderDependency(dep);
+		});
 		callback(null, "module.exports = function() {\n"+
-			"  return new Worker(" + require("webpack/api/getPublicPrefix") + " + " + 
+			"  return " + constructor + "(" +
+				(workerMainContent ? JSON.stringify(workerMainContent) + ", " : "") +
+				require("webpack/api/getPublicPrefix") + " + " +
 				JSON.stringify(options.output.replace(/\[hash\]/g, stats.hash)) + ");\n" + "}");
 	});
 }
