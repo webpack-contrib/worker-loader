6c8b0312c25d68844aed19746d135288f514acd3
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2017-10-12 16:38:05 +0200

    chore(defaults): v1.4.0...1.6.0

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-/* eslint-disable linebreak-style */
-
+/* eslint-disable
+  linebreak-style,
+  import/first,
+  import/order,
+  no-param-reassign,
+  no-underscore-dangle
+*/
 import path from 'path';
+
+import schema from './options.json';
 import loaderUtils from 'loader-utils';
 import validateOptions from 'schema-utils';
-import WebWorkerTemplatePlugin from 'webpack/lib/webworker/WebWorkerTemplatePlugin';
+
 import NodeTargetPlugin from 'webpack/lib/node/NodeTargetPlugin';
 import SingleEntryPlugin from 'webpack/lib/SingleEntryPlugin';
-import schema from '../options.json';
+import WebWorkerTemplatePlugin from 'webpack/lib/webworker/WebWorkerTemplatePlugin';
 
 const getWorker = (file, content, options) => {
   const workerPublicPath = `__webpack_public_path__ + ${JSON.stringify(file)}`;
+
   if (options.inline) {
     const createInlineWorkerPath = JSON.stringify(`!!${path.join(__dirname, '..', 'createInlineWorker.js')}`);
     const fallbackWorkerPath = options.fallback === false ? 'null' : workerPublicPath;
+
     return `require(${createInlineWorkerPath})(${JSON.stringify(content)}, ${fallbackWorkerPath})`;
   }
+
   return `new Worker(${workerPublicPath})`;
 };
 
@@ -22,7 +32,9 @@ export default function workerLoader() {}
 
 workerLoader.pitch = function pitch(request) {
   if (!this.webpack) throw new Error('Only usable with webpack');
+
   this.cacheable(false);
+
   const callback = this.async();
   const options = loaderUtils.getOptions(this) || {};
 
@@ -32,50 +44,63 @@ workerLoader.pitch = function pitch(request) {
     context: options.context || this.options.context,
     regExp: options.regExp,
   });
+
   const outputOptions = {
     filename,
     chunkFilename: `[id].${filename}`,
     namedChunkFilename: null,
   };
+
   if (this.options && this.options.worker && this.options.worker.output) {
     Object.keys(this.options.worker.output).forEach((name) => {
       outputOptions[name] = this.options.worker.output[name];
     });
   }
-  /* eslint-disable no-underscore-dangle */
-  const workerCompiler = this._compilation.createChildCompiler('worker', outputOptions);
-  /* eslint-enable no-underscore-dangle */
+
+  const workerCompiler = this._compilation
+    .createChildCompiler('worker', outputOptions);
+
   workerCompiler.apply(new WebWorkerTemplatePlugin(outputOptions));
+
   if (this.target !== 'webworker' && this.target !== 'web') {
     workerCompiler.apply(new NodeTargetPlugin());
   }
+
   workerCompiler.apply(new SingleEntryPlugin(this.context, `!!${request}`, 'main'));
+
   if (this.options && this.options.worker && this.options.worker.plugins) {
     this.options.worker.plugins.forEach(plugin => workerCompiler.apply(plugin));
   }
+
   const subCache = `subcache ${__dirname} ${request}`;
+
   workerCompiler.plugin('compilation', (compilation) => {
-    /* eslint-disable no-param-reassign */
     if (compilation.cache) {
-      if (!compilation.cache[subCache]) {
-        compilation.cache[subCache] = {};
-      }
+      if (!compilation.cache[subCache]) compilation.cache[subCache] = {};
+
       compilation.cache = compilation.cache[subCache];
     }
-    /* eslint-enable no-param-reassign */
   });
+
   workerCompiler.runAsChild((err, entries, compilation) => {
     if (err) return callback(err);
+
     if (entries[0]) {
       const [workerFile] = entries[0].files;
-      const workerFactory = getWorker(workerFile, compilation.assets[workerFile].source(), options);
+
+      const workerFactory = getWorker(
+        workerFile,
+        compilation.assets[workerFile].source(),
+        options
+      );
+
       if (options.fallback === false) {
-        /* eslint-disable no-underscore-dangle */
         delete this._compilation.assets[workerFile];
-        /* eslint-enable no-underscore-dangle */
       }
+
       return callback(null, `module.exports = function() {\n\treturn ${workerFactory};\n};`);
     }
+
     return callback(null, null);
   });
 };
--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
-/* eslint-disable linebreak-style */
-
-import assert from 'assert';
+/* eslint-disable linebreak-style, import/order */
 import fs from 'fs';
-import path from 'path';
 import del from 'del';
+import path from 'path';
+import assert from 'assert';
 import webpack from 'webpack';
 
 process.chdir(__dirname);
@@ -18,16 +17,18 @@ const makeBundle = (name, options) => del(`expected/${name}`).then(() => {
       filename: 'bundle.js',
     },
   }, options);
+
   const bundle = webpack(config);
+
   return new Promise((resolve, reject) => {
     bundle.run((err, stats) => {
-      if (err) {
-        reject(err);
-      } else if (stats.compilation.errors.length) {
+      if (err) reject(err);
+
+      if (stats.compilation.errors.length) {
         reject(Error(stats.toString('errors-only')));
-      } else {
-        resolve(stats);
       }
+
+      resolve(stats);
     });
   });
 });
@@ -39,9 +40,11 @@ test('should create chunk with worker', () =>
       .reduce((acc, item) => acc.concat(item), [])
       .map(item => item.files)
       .map(item => `expected/worker/${item}`);
+
     assert.equal(files.length, 1);
+
     assert.notEqual(readFile(files[0]).indexOf('// worker test mark'), -1);
-  }),
+  })
 );
 
 test('should create chunk with specified name in query', () =>
@@ -51,9 +54,11 @@ test('should create chunk with specified name in query', () =>
       .reduce((acc, item) => acc.concat(item), [])
       .map(item => item.files)
       .map(item => `expected/name-query/${item}`);
+
     assert.equal(files[0], 'expected/name-query/namedWorker.js');
+
     assert.notEqual(readFile(files[0]).indexOf('// named worker test mark'), -1);
-  }),
+  })
 );
 
 test('should create named chunks with workers via options', () =>
@@ -76,12 +81,14 @@ test('should create named chunks with workers via options', () =>
       .map(item => item.files)
       .map(item => `expected/name-options/${item}`)
       .sort();
+
     assert.equal(files.length, 2);
     assert.equal(files[0], 'expected/name-options/w1.js');
     assert.equal(files[1], 'expected/name-options/w2.js');
+
     assert.notEqual(readFile(files[0]).indexOf('// w1 via worker options'), -1);
     assert.notEqual(readFile(files[1]).indexOf('// w2 via worker options'), -1);
-  }),
+  })
 );
 
 test('should inline worker with inline option in query', () =>
@@ -90,9 +97,11 @@ test('should inline worker with inline option in query', () =>
       .map(item => item.files)
       .reduce((acc, item) => acc.concat(item), [])
       .map(item => `expected/inline-query/${item}`);
+
     assert.equal(files.length, 1);
+
     assert.notEqual(readFile(files[0]).indexOf('// inlined worker test mark'), -1);
-  }),
+  })
 );
 
 test('should inline worker with inline in options', () =>
@@ -113,10 +122,12 @@ test('should inline worker with inline in options', () =>
       .map(item => item.files)
       .reduce((acc, item) => acc.concat(item), [])
       .map(item => `expected/inline-options/${item}`);
+
     assert.equal(files.length, 1);
+
     assert.notEqual(readFile(files[0]).indexOf('// w1 inlined via options'), -1);
     assert.notEqual(readFile(files[0]).indexOf('// w2 inlined via options'), -1);
-  }),
+  })
 );
 
 test('should add fallback chunks with inline option', () =>
@@ -138,16 +149,20 @@ test('should add fallback chunks with inline option', () =>
       .reduce((acc, item) => acc.concat(item), [])
       .map(item => item.files)
       .map(item => `expected/inline-fallbacks/${item}`);
+
     assert.equal(files.length, 2);
+
     const w1 = readFile(files[0]);
     const w2 = readFile(files[1]);
+
     if (w1.indexOf('// w1 via worker options') !== -1) {
       assert.notEqual(w2.indexOf('// w2 via worker options'), -1);
     }
+
     if (w1.indexOf('// w2 via worker options') !== -1) {
       assert.notEqual(w2.indexOf('// w1 via worker options'), -1);
     }
-  }),
+  })
 );
 
 test('should not add fallback chunks with inline and fallback === false', () =>
@@ -169,11 +184,14 @@ test('should not add fallback chunks with inline and fallback === false', () =>
       .map(item => item.files)
       .reduce((acc, item) => acc.concat(item), [])
       .map(item => `expected/no-fallbacks/${item}`);
+
     assert(bundleFile);
+
     assert.equal(fs.readdirSync('expected/no-fallbacks').length, 1);
+
     assert.notEqual(readFile(bundleFile).indexOf('// w1 inlined without fallback'), -1);
     assert.notEqual(readFile(bundleFile).indexOf('// w2 inlined without fallback'), -1);
-  }),
+  })
 );
 
 ['web', 'webworker'].forEach((target) => {
@@ -194,11 +212,19 @@ test('should not add fallback chunks with inline and fallback === false', () =>
       },
     }).then((stats) => {
       assert.notEqual(stats.compilation.missingDependencies.length, 0);
-    }),
+    })
   );
 });
 
-['node', 'async-node', 'node-webkit', 'atom', 'electron', 'electron-main', 'electron-renderer'].forEach((target) => {
+[
+  'node',
+  'async-node',
+  'node-webkit',
+  'atom',
+  'electron',
+  'electron-main',
+  'electron-renderer',
+].forEach((target) => {
   test(`should not have missing dependencies (${target})`, () =>
     makeBundle('nodejs-core-modules', {
       target,
@@ -216,6 +242,6 @@ test('should not add fallback chunks with inline and fallback === false', () =>
       },
     }).then((stats) => {
       assert.equal(stats.compilation.missingDependencies.length, 0);
-    }),
+    })
   );
 });
