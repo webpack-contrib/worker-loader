33f14e746b5f7d6c438728852a26d60f6659a30b
nick.feng <nick.feng@coupang.com>
2020-05-22 16:47:38 +0800

    support wasm

--- src/index.js
+++ src/index.js
@@ -66,6 +66,26 @@ export function pitch(request) {
     new NodeTargetPlugin().apply(worker.compiler);
   }
 
+  // add wasm support
+  let wasmPluginPath = null;
+	try {
+		wasmPluginPath = require.resolve(
+			'webpack/lib/web/FetchCompileWasmTemplatePlugin'
+		);
+	}
+	catch (_err) {
+		// webpack <= v3, skipping
+	}
+
+	if (wasmPluginPath) {
+		// eslint-disable-next-line global-require
+		const FetchCompileWasmTemplatePlugin = require(wasmPluginPath);
+		new FetchCompileWasmTemplatePlugin({
+			mangleImports: this._compiler.options.optimization.mangleWasmImports
+		}).apply(worker.compiler);
+	}
+
+
   new SingleEntryPlugin(this.context, `!!${request}`, 'main').apply(
     worker.compiler
   );
