ee519b1d283dbb599385fe2932c99c929b09db36
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2020-08-05 19:12:36 +0300

    fix: compatibility with webpack@5 cache (#279)

--- src/supportWebpack5.js
+++ src/supportWebpack5.js
@@ -1,5 +1,3 @@
-import { stringifyRequest } from 'loader-utils';
-
 import { workerGenerator, sourceMappingURLRegex } from './utils';
 
 export default function runAsChild(
@@ -8,9 +6,6 @@ export default function runAsChild(
   options,
   callback
 ) {
-  // eslint-disable-next-line import/no-unresolved, global-require
-  const getLazyHashedEtag = require('webpack/lib/cache/getLazyHashedEtag');
-
   workerContext.compiler.runAsChild((error, entries, compilation) => {
     if (error) {
       return callback(error);
@@ -18,68 +13,66 @@ export default function runAsChild(
 
     if (entries[0]) {
       const [workerFilename] = [...entries[0].files];
-      const requestIdent = stringifyRequest(
-        { context: loaderContext.rootContext },
-        workerContext.request
+      const cache = workerContext.compiler.getCache('worker-loader');
+      const cacheIdent = workerFilename;
+      const cacheETag = cache.getLazyHashedEtag(
+        compilation.assets[workerFilename]
       );
-      const cacheIdent = `${workerContext.compiler.compilerPath}/worker-loader|${requestIdent}`;
-      const cacheETag = getLazyHashedEtag(compilation.assets[workerFilename]);
 
-      // TODO not working, need fix on webpack@5 side
-      return workerContext.compiler.cache.get(
-        cacheIdent,
-        cacheETag,
-        (getCacheError, content) => {
-          if (getCacheError) {
-            return callback(getCacheError);
-          }
-
-          if (options.inline === 'no-fallback') {
-            // eslint-disable-next-line no-underscore-dangle, no-param-reassign
-            delete loaderContext._compilation.assets[workerFilename];
-
-            // TODO improve this, we should store generated source maps files for file in `assetInfo`
-            // eslint-disable-next-line no-underscore-dangle
-            if (loaderContext._compilation.assets[`${workerFilename}.map`]) {
-              // eslint-disable-next-line no-underscore-dangle, no-param-reassign
-              delete loaderContext._compilation.assets[`${workerFilename}.map`];
-            }
-          }
-
-          if (content) {
-            return callback(null, content);
-          }
-
-          let workerSource = compilation.assets[workerFilename].source();
-
-          if (options.inline === 'no-fallback') {
-            // Remove `/* sourceMappingURL=url */` comment
-            workerSource = workerSource.replace(sourceMappingURLRegex, '');
-          }
-
-          const workerCode = workerGenerator(
-            loaderContext,
-            workerFilename,
-            workerSource,
-            options
-          );
-
-          return workerContext.compiler.cache.store(
-            cacheIdent,
-            cacheETag,
-            workerCode,
-            (storeCacheError) => {
-              if (storeCacheError) {
-                return callback(storeCacheError);
-              }
-
-              return callback(null, workerCode);
-            }
-          );
+      return cache.get(cacheIdent, cacheETag, (getCacheError, content) => {
+        if (getCacheError) {
+          return callback(getCacheError);
         }
-      );
+
+        if (options.inline === 'no-fallback') {
+          // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+          delete loaderContext._compilation.assets[workerFilename];
+
+          // TODO improve this, we should store generated source maps files for file in `assetInfo`
+          // eslint-disable-next-line no-underscore-dangle
+          if (loaderContext._compilation.assets[`${workerFilename}.map`]) {
+            // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+            delete loaderContext._compilation.assets[`${workerFilename}.map`];
+          }
+        }
+
+        if (content) {
+          return callback(null, content);
+        }
+
+        let workerSource = compilation.assets[workerFilename].source();
+
+        if (options.inline === 'no-fallback') {
+          // Remove `/* sourceMappingURL=url */` comment
+          workerSource = workerSource.replace(sourceMappingURLRegex, '');
+        }
+
+        const workerCode = workerGenerator(
+          loaderContext,
+          workerFilename,
+          workerSource,
+          options
+        );
+
+        return cache.store(
+          cacheIdent,
+          cacheETag,
+          workerCode,
+          (storeCacheError) => {
+            if (storeCacheError) {
+              return callback(storeCacheError);
+            }
+
+            return callback(null, workerCode);
+          }
+        );
+      });
     }
 
-    return callback(null, null);
+    return callback(
+      new Error(
+        `Failed to compile web worker "${workerContext.request}" request`
+      )
+    );
   });
 }
